8d4824b6a958532f5a0b4c631cb9cc7ee81be999
Rahul <apple@DS4USYS-IOS.local>
2014-09-24 16:05:39 +0530

    Initial Commit

---
+++ Movie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Movie
+//
+//  Created by Rahul on 24/09/14.
+//  Copyright (c) 2014 RahulSaini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Movie/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Movie
+//
+//  Created by Rahul on 24/09/14.
+//  Copyright (c) 2014 RahulSaini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
